7929992387450ccf30ea0061187aa793ba84e763
amandalchang <113650953+amandalchang@users.noreply.github.com>
2026-04-24 02:25:39 -0400

    MCP reads working, writes not

--- Firmware/Core/core.ino
+++ Firmware/Core/core.ino
@@ -1,112 +1,106 @@
 #define HAL_FDCAN_MODULE_ENABLED
 #define HAL_CORTEX_MODULE_ENABLED
 
+
 #include "stm32g4xx_hal.h"
 #include "stm32g4xx_hal_fdcan.h"
 #include "can_api.h"
-#include "ADBMS181x.h"
-#include "bms_hardware.h"
-#include <SPI.h>
-#include <Wire.h>
-#include <math.h>
+#include "MCP23S17.h"
 
-#define LED PB9
-#define TOTAL_IC 1
+// STM32G473CCTx pin defs
+#define HEARTBEAT PA8
+#define ASENSE_5V PA0
+#define ASENSE_3V3 PA1
 
-#define THERM_A1_PIN PA0
-#define THERM_B1_PIN PA1
-#define THERM_A2_PIN PA2
-#define THERM_B2_PIN PA3
+#define SHDN_N PB0 
+#define SHDN_NE PB1 
+#define SHDN_E PB2 
+#define SHDN_SE PB3 
+#define SHDN_S PB4 
+#define SHDN_SW PB5 
+#define SHDN_W PB9 
+#define SHDN_NW PB10 
 
-#define MUX_SDA_PIN PA8
-#define MUX_SCL_PIN PA9
+#define RESET_IO PA10
 
-#define MUX1_ADDR 0x4C
-#define MUX2_ADDR 0x4E
+#define CAN_COMS PB6 
+#define USBC_COMS PB7 
+#define BOOT0 PB8 
 
-#define MAX14661_CMD_A 0x14
-#define MAX14661_CMD_B 0x15
+#define CUBE_HANDOFF PB11 
+#define RXCAN PB12 
+#define TXCAN PB13 
 
-cell_asic bms_ic[TOTAL_IC];
+#define CUBE_IO_RESET PB14 
+#define CTRL_NONCRIT_CHECK PB15 
+#define SENSE_POWER_CYCLE PA15
+
+// MCP Pin Defs
+// port B; all unlabeled are input
+#define RESET_CUBE_1 8 // out
+#define SHDN_REMOTE 9
+#define SHDN_TILT 10
+#define SHDN_WIRED 11
+#define SHDN_FRAME 12
+#define SHDN_TSMS 13
+#define SHDN_CRIT_RIDER 14
+#define SHDN_CTRL_LOBATT 15
+
+// port A; all unlabeled are input
+#define DEBUG1 0 // out
+#define RESET_CUBE_2 1 // out
+#define SHDN_LOWER_RIDER 3
+#define SHDN_NUC 2
+#define SHDN_BMS_N 4
+#define SHDN_BMS_E 5
+#define SHDN_BMS_S 6
+#define SHDN_BMS_ALL 7
+
 FDCAN_HandleTypeDef hfdcan2;
 
-enum BmsState {
-  IDLE,
-  FAULTED
-};
+// CS, MISO, MOSI, SCK
+#define CS PA4
+#define MISO PA6
+#define MOSI PA7
+#define SCK PA5
 
-enum BmsFaultBits {
-  FAULT_ADBMS_COMMS    = 2,
-  FAULT_MUX            = 4,
-  FAULT_CELL_UNDERTEMP = 5,
-  FAULT_CELL_OVERTEMP  = 6,
-  FAULT_CELL_UNDERVOLT = 7,
-  FAULT_CELL_OVERVOLT  = 8,
-  FAULT_PEC            = 15
-};
+MCP23S17 MCP(CS);
 
-bms_status_a.bms_state = IDLE;
-uint16_t bms_faults = 0;
-
-const float CELL_VOLTAGE_LOW  = 2.5f; // Min value is can handle is 2.5(discharge)
-const float CELL_VOLTAGE_HIGH = 4.25f; // Max value it can handle is 4.25(discharge)
-
-//Must add the over current later aswell
-const float TEMP_LOW_C  = -20.0f; // Min value it cna take is -20.0f(discharge)
-const float TEMP_HIGH_C = 60.0f; // Max value it can tolerate = 55.0f(discharge)
-
-// Stored measurement summaries
-float min_cell_voltage = 100.0f;
-float max_cell_voltage = 0.0f;
-float pack_voltage = 0.0f;
-
-float min_temp_c = 1000.0f;
-float max_temp_c = -1000.0f;
-
-int8_t last_pec_error = 0;
-bool last_mux_error = false;
-
-const char *mux1_names[16] = {
-  "TH_1_A", "TH_1_B", "TH_1_C", "TH_1_D",
-  "TH_2_A", "TH_2_B", "TH_2_C", "TH_2_D",
-  "TH_3_C", "TH_3_D", "TH_3_A", "TH_3_B",
-  "TH_4_A", "TH_4_B", "TH_4_C", "TH_4_D"
-};
-
-const char *mux2_names[16] = {
-  "TH_5_A", "TH_5_B", "TH_5_C", "TH_5_D",
-  "TH_6_A", "TH_6_B", "TH_6_C", "TH_6_D",
-  "TH_7_A", "TH_7_B", "TH_7_C", "TH_7_D",
-  "THERM_BUSBAR", "THERM_AMBIENT", "UNUSED_15", "UNUSED_16"
-};
+bool mcpbegin = 0;
 
 void OCTAL_Set_Clock(void) {
+  // set flash latency for target (144MHz -> FLASH_LATENCY_4) and up voltage to HF range
   __HAL_RCC_PWR_CLK_ENABLE();
   HAL_PWREx_ControlVoltageScaling(PWR_REGULATOR_VOLTAGE_SCALE1_BOOST);
 
+  // Request HSE ON (clock source mode from external high-speed crystal)
   RCC->CR |= RCC_CR_HSEON;
+
+  //Move PLL to source off of HSE
   MODIFY_REG(RCC->CFGR, RCC_CFGR_SW, RCC_CFGR_SW_HSE);
 
-  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  //Setup PLL for 48MHz peripheral clock
+  RCC_OscInitTypeDef RCC_OscInitStruct = { 0 };
   RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
   RCC_OscInitStruct.HSEState = RCC_HSE_ON;
   RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
   RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
-  RCC_OscInitStruct.PLL.PLLM = RCC_PLLM_DIV4;
-  RCC_OscInitStruct.PLL.PLLN = 24;
-  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
-  RCC_OscInitStruct.PLL.PLLQ = RCC_PLLQ_DIV6;
+  RCC_OscInitStruct.PLL.PLLM = RCC_PLLM_DIV4;  // 12MHz after div
+  RCC_OscInitStruct.PLL.PLLN = 24;             //2ds88Mhz after mult
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;  //144MHz CPU clock
+  RCC_OscInitStruct.PLL.PLLQ = RCC_PLLQ_DIV6;  //Peripherals at 48MHz (required by USB (48MHz only), CAN ahas a bit more leeway)
   RCC_OscInitStruct.PLL.PLLR = RCC_PLLR_DIV2;
-
   if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK) {
     Error_Handler();
   }
 
+  //Enable PLL
   SET_BIT(RCC->CR, RCC_CR_PLLON);
 
-  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+  //Set CPU and bus clocks to source 144MHz from PLL
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = { 0 };
   RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK
-                              | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
+                                | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
   RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
   RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
   RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
@@ -116,282 +110,245 @@ void OCTAL_Set_Clock(void) {
     Error_Handler();
   }
 
+  //push new clock settings to time-dependent code/structs
   SystemCoreClockUpdate();
+
+  //Start tick counter
   HAL_InitTick(TICK_INT_PRIORITY);
 }
 
-void init_bms_chip_array()
-{
-  for (int i = 0; i < TOTAL_IC; i++)
-  {
-    bms_ic[i].isospi_reverse = false;
-
-    bms_ic[i].ic_reg.cell_channels = 18;
-    bms_ic[i].ic_reg.stat_channels = 4;
-    bms_ic[i].ic_reg.aux_channels = 9;
-
-    bms_ic[i].ic_reg.num_cv_reg = 6;
-    bms_ic[i].ic_reg.num_gpio_reg = 4;
-    bms_ic[i].ic_reg.num_stat_reg = 2;
-
-    bms_ic[i].system_open_wire = 0;
-
-    for (int j = 0; j < 18; j++)
-    {
-      bms_ic[i].cells.c_codes[j] = 0;
-    }
-
-    for (int j = 0; j < 6; j++)
-    {
-      bms_ic[i].cells.pec_match[j] = 0;
-    }
-  }
-
-  ADBMS181x_init_cfg(TOTAL_IC, bms_ic);
-  ADBMS181x_reset_crc_count(TOTAL_IC, bms_ic);
-}
-
-void init_thermistors()
-{
-  pinMode(THERM_A1_PIN, INPUT_ANALOG);
-  pinMode(THERM_B1_PIN, INPUT_ANALOG);
-  pinMode(THERM_A2_PIN, INPUT_ANALOG);
-  pinMode(THERM_B2_PIN, INPUT_ANALOG);
-
-  Wire.setSDA(MUX_SDA_PIN);
-  Wire.setSCL(MUX_SCL_PIN);
-  Wire.begin();
-  Wire.setClock(100000);
-
-  analogReadResolution(12);
-}
-
-bool set_mux_channels(uint8_t mux_addr, uint8_t chA, uint8_t chB)
-{
-  if (chA > 0x10 || chB > 0x10) return false;
-
-  Wire.beginTransmission(mux_addr);
-  Wire.write(MAX14661_CMD_A);
-  Wire.write(chA);
-  Wire.write(chB);
-  return (Wire.endTransmission() == 0);
-}
-
-float raw_to_therm_voltage(uint16_t raw)
-{
-  return (3.3f * raw) / 4095.0f;
-}
-
-float therm_voltage_to_temp_c(float v_out)
-{
-  if (v_out <= 0.0f || v_out >= 3.3f) {
-    return -1000.0f;
-  }
-
-  float temp_k = 1.0f / (
-    (1.0f / 298.15f) +
-    (1.0f / 4250.0f) * log(v_out / (3.3f - v_out))
-  );
-
-  return temp_k - 273.15f;
-}
-
-void set_fault_bit(uint8_t bit)
-{
-  bms_faults |= (1U << bit);
-}
-
-bool read_cell_voltages()
-{
-  min_cell_voltage = 100.0f;
-  max_cell_voltage = 0.0f;
-  pack_voltage = 0.0f;
-
-  wakeup_idle(TOTAL_IC);
-  ADBMS181x_adcv(MD_7KHZ_3KHZ, DCP_DISABLED, CELL_CH_ALL);
-  ADBMS181x_pollAdc();
-
-  last_pec_error = ADBMS181x_rdcv(0, TOTAL_IC, bms_ic);
-
-  for (int ic = 0; ic < TOTAL_IC; ic++)
-  {
-    for (int cell = 0; cell < 18; cell++)
-    {
-      float volts = bms_ic[ic].cells.c_codes[cell] * 0.0001f;
-
-      if (volts < min_cell_voltage) min_cell_voltage = volts;
-      if (volts > max_cell_voltage) max_cell_voltage = volts;
-
-      pack_voltage += volts;
-    }
-  }
-
-  return (last_pec_error == 0);
-}
-
-bool read_all_thermistors()
-{
-  min_temp_c = 1000.0f;
-  max_temp_c = -1000.0f;
-  last_mux_error = false;
-
-  for (uint8_t index = 0; index < 16; index++)
-  {
-    bool mux1_ok = set_mux_channels(MUX1_ADDR, index, index);
-    bool mux2_ok = set_mux_channels(MUX2_ADDR, index, index);
-
-    if (!mux1_ok || !mux2_ok)
-    {
-      last_mux_error = true;
-      return false;
-    }
-
-    delay(5);
-
-    float t1 = therm_voltage_to_temp_c(raw_to_therm_voltage(analogRead(THERM_A1_PIN)));
-    float t2 = therm_voltage_to_temp_c(raw_to_therm_voltage(analogRead(THERM_B1_PIN)));
-    float t3 = therm_voltage_to_temp_c(raw_to_therm_voltage(analogRead(THERM_A2_PIN)));
-    float t4 = therm_voltage_to_temp_c(raw_to_therm_voltage(analogRead(THERM_B2_PIN)));
-
-    if (t1 < min_temp_c) min_temp_c = t1;
-    if (t2 < min_temp_c) min_temp_c = t2;
-    if (t3 < min_temp_c) min_temp_c = t3;
-    if (t4 < min_temp_c) min_temp_c = t4;
-
-    if (t1 > max_temp_c) max_temp_c = t1;
-    if (t2 > max_temp_c) max_temp_c = t2;
-    if (t3 > max_temp_c) max_temp_c = t3;
-    if (t4 > max_temp_c) max_temp_c = t4;
-
-    delay(20);
-  }
-
-  return true;
-}
-
-void update_faults()
-{
-  bms_faults = 0;
-
-  if (last_pec_error != 0)
-  {
-    set_fault_bit(FAULT_ADBMS_COMMS);
-    set_fault_bit(FAULT_PEC);
-  }
-
-  if (last_mux_error)
-  {
-    set_fault_bit(FAULT_MUX);
-  }
-
-  if (min_cell_voltage < CELL_VOLTAGE_LOW)
-  {
-    set_fault_bit(FAULT_CELL_UNDERVOLT);
-  }
-
-  if (max_cell_voltage > CELL_VOLTAGE_HIGH)
-  {
-    set_fault_bit(FAULT_CELL_OVERVOLT);
-  }
-
-  if (min_temp_c < TEMP_LOW_C)
-  {
-    set_fault_bit(FAULT_CELL_UNDERTEMP);
-  }
-
-  if (max_temp_c > TEMP_HIGH_C)
-  {
-    set_fault_bit(FAULT_CELL_OVERTEMP);
-  }
-}
-
-void print_faults()
-{
-  Serial.print("Fault mask: 0b");
-  Serial.println(bms_faults, BIN);
-  if (bms_faults & (1U << FAULT_ADBMS_COMMS))    Serial.println("FAULT: ADBMS comms");
-  if (bms_faults & (1U << FAULT_MUX))            Serial.println("FAULT: Mux");
-  if (bms_faults & (1U << FAULT_PEC))            Serial.println("FAULT: PEC");
-  if (bms_faults & (1U << FAULT_CELL_UNDERVOLT)) Serial.println("FAULT: Cell undervoltage");
-  if (bms_faults & (1U << FAULT_CELL_OVERVOLT))  Serial.println("FAULT: Cell overvoltage");
-  if (bms_faults & (1U << FAULT_CELL_UNDERTEMP)) Serial.println("FAULT: Cell undertemp");
-  if (bms_faults & (1U << FAULT_CELL_OVERTEMP))  Serial.println("FAULT: Cell overtemp");
-}
-
-void setup()
-{
+void setup() {
   OCTAL_Set_Clock();
 
-  pinMode(LED, OUTPUT);
-  pinMode(PB1, OUTPUT);
-  pinMode(PB2, OUTPUT);
-  pinMode(PB3, OUTPUT);
-  pinMode(PB4, OUTPUT);
-  pinMode(PB5, OUTPUT);
-  pinMode(PB6, OUTPUT);
-  pinMode(PB7, OUTPUT);
-  pinMode(PB9, OUTPUT);
-  pinMode(PB11, OUTPUT);
-  pinMode(PB14, OUTPUT);
-  pinMode(PB15, OUTPUT);
-  pinMode(PA15, OUTPUT);
-  pinMode(CS_PIN, OUTPUT);
-  digitalWrite(CS_PIN, HIGH);
+  pinMode(SHDN_N, INPUT);
+  pinMode(SHDN_NE, INPUT);
+  pinMode(SHDN_E, INPUT);
+  pinMode(SHDN_SE, INPUT);
+  pinMode(SHDN_S, INPUT);
+  pinMode(SHDN_SW, INPUT);
+  pinMode(SHDN_W, INPUT);
+  pinMode(SHDN_NW, INPUT);
 
-  digitalToggle(PB9);
-  digitalToggle(PB2);
+  pinMode(CAN_COMS, OUTPUT);
+  pinMode(USBC_COMS, OUTPUT);
+  pinMode(BOOT0, INPUT);
 
-  Serial.begin(115200);
-  delay(200);
+  pinMode(RESET_IO, OUTPUT);
+
+  pinMode(CUBE_HANDOFF, OUTPUT);
+  pinMode(RXCAN, INPUT);
+  pinMode(TXCAN, OUTPUT);
+
+  pinMode(CUBE_IO_RESET, OUTPUT);
+  pinMode(CTRL_NONCRIT_CHECK, OUTPUT);
+  pinMode(SENSE_POWER_CYCLE, OUTPUT);
+
+  pinMode(HEARTBEAT, OUTPUT);  // Core Heartbeat
+
+  Serial.begin(112500);
 
   SPI.begin();
-  SPI.beginTransaction(SPISettings(1000000, MSBFIRST, SPI_MODE0));
+  mcpbegin = MCP.begin();
+  delay(50);
+  if (mcpbegin) {
+    // GPA outputs: DEBUG1, RESET_CUBEs_2
+    MCP.pinMode1(DEBUG1, OUTPUT);
+    MCP.pinMode1(RESET_CUBE_2, OUTPUT);
 
-  init_bms_chip_array();
-  init_thermistors();
+    // GPB output: RESET_CUBE_1
+    MCP.pinMode1(RESET_CUBE_1, OUTPUT);
 
-  can_init_bms_a(&hfdcan2);
+    // everything else input (SHDN signals)
+    MCP.pinMode1(SHDN_REMOTE, INPUT);
+    MCP.pinMode1(SHDN_TILT, INPUT);
+    MCP.pinMode1(SHDN_WIRED, INPUT);
+    MCP.pinMode1(SHDN_FRAME, INPUT);
+    MCP.pinMode1(SHDN_TSMS, INPUT);
+    MCP.pinMode1(SHDN_CRIT_RIDER, INPUT);
+    MCP.pinMode1(SHDN_CTRL_LOBATT, INPUT);
+
+    MCP.pinMode1(SHDN_LOWER_RIDER, INPUT);
+    MCP.pinMode1(SHDN_NUC, INPUT);
+    MCP.pinMode1(SHDN_BMS_N, INPUT);
+    MCP.pinMode1(SHDN_BMS_E, INPUT);
+    MCP.pinMode1(SHDN_BMS_S, INPUT);
+    MCP.pinMode1(SHDN_BMS_ALL, INPUT);
+
+
+    // // Port 0 (GPA): Debug GPA0, ResetCube2 GPA1 are Outputs (0), others are SHDN Inputs (1)
+    // MCP.pinMode8(0, 0b11111100); 
+    
+    // // Port 1 (GPB): ResetCube1 GPB0 is Output (0), others are SHDN Inputs (1)
+    // MCP.pinMode8(1, 0b11111110); 
+
+    MCP.write1(DEBUG1, HIGH); // prime output
+    MCP.write1(RESET_CUBE_2, LOW); // prime output
+    MCP.write1(RESET_CUBE_2, LOW); // prime output
+    
+    Serial.println("MCP23S17 Initialized.");
+  } else {
+    Serial.println("MCP23S17 not found.");
+  }
+  delay(100);
+
+
+  can_init_core(&hfdcan2);
 }
 
-void loop()
-{
-  switch (bms_state)
-  {
-    case IDLE:
-    {
-      bool cells_ok = read_cell_voltages();
-      bool therms_ok = read_all_thermistors();
+uint16_t parsedValue = 0;
+float current_5v = 0;
+float current_3v3 = 0;
 
-      update_faults();
+const int shdnPins[] = {
+  SHDN_N, SHDN_NE, SHDN_E, SHDN_SE, 
+  SHDN_S, SHDN_SW, SHDN_W, SHDN_NW
+};
+const char* shdnLabels[] = {
+  "N ", "NE", "E ", "SE", 
+  "S ", "SW", "W ", "NW"
+};
 
-      Serial.println("BMS state: IDLE");
-      Serial.print("Min cell voltage: ");
-      Serial.println(min_cell_voltage, 4);
-      Serial.print("Max cell voltage: ");
-      Serial.println(max_cell_voltage, 4);
-      Serial.print("Min temp C: ");
-      Serial.println(min_temp_c, 2);
-      Serial.print("Max temp C: ");
-      Serial.println(max_temp_c, 2);
+void printShutdownStatus() {
+    if (mcpbegin) {
+    Serial.println("--- MCP SHDN Status ---");
 
-      if (!cells_ok || !therms_ok || bms_faults != 0)
-      {
-        bms_state = FAULTED;
+    const int mcpPins[] = {
+      SHDN_REMOTE,
+      SHDN_TILT,
+      SHDN_WIRED,
+      SHDN_FRAME,
+      SHDN_TSMS,
+      SHDN_CRIT_RIDER,
+      SHDN_CTRL_LOBATT,
+
+      SHDN_LOWER_RIDER,
+      SHDN_NUC,
+      SHDN_BMS_N,
+      SHDN_BMS_E,
+      SHDN_BMS_S,
+      SHDN_BMS_ALL
+    };
+
+    const char* mcpLabels[] = {
+      "REMOTE",
+      "TILT",
+      "WIRED",
+      "FRAME",
+      "TSMS",
+      "CRIT",
+      "LOBATT",
+
+      "LOWER",
+      "NUC",
+      "BMS_N",
+      "BMS_E",
+      "BMS_S",
+      "BMS_ALL"
+    };
+
+    for (int i = 0; i < 13; i++) {
+      int state = MCP.read1(mcpPins[i]);
+
+      Serial.print(mcpLabels[i]);
+      Serial.print(": ");
+      Serial.print(state == HIGH ? "CLOSED" : "OPEN");
+
+      if ((i + 1) % 4 == 0) {
+        Serial.println();
+      } else {
+        Serial.print(" | ");
       }
-
-      break;
     }
 
-    case FAULTED:
-    {
-      Serial.println("BMS state: FAULTED");
-      print_faults();
-
-      digitalToggle(PB9);
-      delay(500);
-      break;
-    }
+    Serial.println();
   }
 
-  delay(200);
+  Serial.println("--- SHDN Pin Status ---");
+  
+  for (int i = 0; i < 8; i++) {
+    int state = digitalRead(shdnPins[i]);
+    
+    Serial.print(shdnLabels[i]);
+    Serial.print(": ");
+    Serial.print(state == HIGH ? "CLOSED" : "OPEN ");
+    
+    if ((i + 1) % 4 == 0) {
+      Serial.println();
+    } else {
+      Serial.print(" | ");
+    }
+  }
+  Serial.println("-----------------------");
+}
+
+float MAX_VPACK_DIFF = 0.080; // 80mV pack difference
+
+void loop() {
+
+  digitalToggle(HEARTBEAT);
+  MCP.write1(DEBUG1, HIGH);
+  delay(1000);
+
+
+  current_5v = (analogRead(ASENSE_5V) * 3.3) / 1023.0;
+  current_3v3 = (analogRead(ASENSE_3V3) * 3.3) / 1023.0;
+  // Serial.print("Current 5V: ");
+  // Serial.println(current_5v);
+  // Serial.print("Current 3V3: ");
+  // Serial.println(current_3v3);
+
+  // printShutdownStatus();
+
+  delay(100);
+  core_status.core_state = 0;
+  Serial.print("DEBUG1 raw read: ");
+  Serial.println(MCP.read1(DEBUG1), HEX);
+
+  digitalWrite(RESET_IO, HIGH);
+
+  if (Serial.available()) {
+    digitalWrite(USBC_COMS, HIGH);
+    String input = Serial.readStringUntil('\n');
+    input.trim();
+
+    long value = input.toInt();
+
+    if (value >= 0 && value <= 65535) {
+      parsedValue = value;
+
+      Serial.print("Parsed uint16_t value: ");
+      Serial.println(parsedValue, HEX);
+    } else {
+      Serial.println("Value out of uint16_t range.");
+    }
+  } else {
+    digitalWrite(USBC_COMS, LOW);
+  }
+
+  if (can_receive(&hfdcan2) > 0) {
+    Serial.print("CAN RECEIVED");
+    digitalWrite(CAN_COMS, HIGH);
+    
+    // // pack voltage reading
+    // float min_volt = bms_status_n.pack_voltage;
+    // if (bms_status_e.pack_voltage < min_volt) min_volt = bms_status_e.pack_voltage;
+    // if (bms_status_s.pack_voltage < min_volt) min_volt = bms_status_s.pack_voltage;
+    // if (bms_status_w.pack_voltage < min_volt) min_volt = bms_status_w.pack_voltage;
+
+    // core_batt_ctrl.north = (bms_status_n.pack_voltage <= (min_volt + MAX_VPACK_DIFF)) ? 1 : 0;
+    // core_batt_ctrl.east  = (bms_status_e.pack_voltage <= (min_volt + MAX_VPACK_DIFF)) ? 1 : 0;
+    // core_batt_ctrl.south = (bms_status_s.pack_voltage <= (min_volt + MAX_VPACK_DIFF)) ? 1 : 0;
+    // core_batt_ctrl.west  = (bms_status_w.pack_voltage <= (min_volt + MAX_VPACK_DIFF)) ? 1 : 0;
+
+    // if (core_batt_ctrl == 1111) {
+    //   MCP.write1(DEBUG1, HIGH);
+    // }
+    
+    Serial.print(CAN_COMS);
+  } else {
+    digitalWrite(CAN_COMS, LOW);
+  }
+
+  can_send_core_status(&hfdcan2);
+  can_send_core_batt_ctrl(&hfdcan2);
 }
